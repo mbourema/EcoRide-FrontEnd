9f315819322d4c61fc7215ff81ac65d01a33ceef
mehdi <mehdibourema@outlook.fr>
2025-03-03 16:54:52 +0100

    Proposition d'autocomplétion pour les champs de la navbar

--- JS/index.js
+++ JS/index.js
@@ -56,7 +56,7 @@ function validateVille(input) {
         fetch(`https://geo.api.gouv.fr/communes?nom=${query}&fields=nom,code&boost=population&limit=5`)
             .then(response => response.json())
             .then(data => {
-                displayResults(data, resultsContainer, query);
+                displayResults(data, resultsContainer, input);
             })
             .catch(error => console.error("Erreur API:", error));
     } else {
